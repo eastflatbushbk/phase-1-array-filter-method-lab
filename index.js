b858b6748af899dab6c96963d5811fc9109effbb
Robert Sainton <robertsainton@outlook.com>
2022-03-12 22:25:43 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+  
+function findMatching (driver, string) {
+    return  driver.filter( (potentialMatch) => potentialMatch.toUpperCase() === string.toUpperCase() )
+}    
+  
+function fuzzyMatch(driver, string){
+    return driver.filter((potentialMatch) => potentialMatch.toUpperCase().indexOf(string.toUpperCase())=== 0 )
+}
+function matchName(driver, string){
+    return  driver.filter( (potentialMatch) => potentialMatch.name === string )
+}
